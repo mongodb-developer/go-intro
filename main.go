f5e2ecaa664a656b5d8080ae1e759aff92651538
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2024-04-04 17:55:35 +0200

    Connect to MongoDB

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Scope struct {
@@ -19,10 +23,26 @@ type Note struct {
 	Scope Scope
 }
 
+var mdbClient *mongo.Client
+
 func main() {
 	const serverAddr string = "127.0.0.1:8081"
+	// TODO: Replace with your connection string
+	const connStr string = "mongodb+srv://yourusername:yourpassword@notekeeper.xxxxxx.mongodb.net/?retryWrites=true&w=majority&appName=NoteKeeper"
 
 	fmt.Println("Hola Caracola")
+
+	ctxBg := context.Background()
+	var err error
+	mdbClient, err = mongo.Connect(ctxBg, options.Client().ApplyURI(connStr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = mdbClient.Disconnect(ctxBg); err != nil {
+			panic(err)
+		}
+	}()
 
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("HTTP Caracola"))
