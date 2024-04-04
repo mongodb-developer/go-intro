9bdf8d2c1bffa54ae5abd1ee5f47a913e2e5e7af
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2024-04-04 17:43:39 +0200

    Implement simplest web server

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
+	const serverAddr string = "127.0.0.1:8081"
+
 	fmt.Println("Hola Caracola")
+
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("HTTP Caracola"))
+	})
+	log.Fatal(http.ListenAndServe(serverAddr, nil))
 }
