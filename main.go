df8aa115cfc5c104e816a28b859ab7da4a0d8807
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2024-04-04 17:48:41 +0200

    Implement (de)serialization

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Scope struct {
+	Project string
+	Area    string
+}
+
+type Note struct {
+	Title string
+	Tags  []string
+	Text  string
+	Scope Scope
+}
 
 func main() {
 	const serverAddr string = "127.0.0.1:8081"
@@ -14,5 +27,16 @@ func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("HTTP Caracola"))
 	})
+	http.HandleFunc("POST /notes", createNote)
 	log.Fatal(http.ListenAndServe(serverAddr, nil))
+}
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	var note Note
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&note); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Note: %+v", note)
 }
